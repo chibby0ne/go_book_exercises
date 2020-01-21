e13d78999fa00c99ab5df88199ff877368e3fe76
Antonio Gutierrez <chibby0ne@gmail.com>
2020-01-21 22:21:31 +0100

    ch5: add ex5_17
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

---
+++ chapter5/exercise5_17/exercise5_17.go
@@ -0,0 +1,60 @@
+// Write a variadic function ElementsByTagName that, given an HTML node tree and zero or more names,
+// returns all the elements that match one of those names. Here are two example calls:
+//
+// func Elements ByTagName(doc *html.Node, name ...string) []*html.Node
+//
+// images := ElementsByTagName(doc, "img")
+// headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
+
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"os"
+)
+
+func ElementsByTagName(doc *html.Node, name ...string) []*html.Node {
+	var nodes []*html.Node
+	visited := make(map[*html.Node]bool)
+	stack := []*html.Node{doc}
+	for len(stack) != 0 {
+		c := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if !visited[c] {
+			visited[c] = true
+			for n := c.FirstChild; n != nil; n = n.NextSibling {
+				stack = append(stack, n)
+			}
+		}
+		if c.Type == html.ElementNode {
+			for _, tag := range name {
+				if c.Data == tag {
+					nodes = append(nodes, c)
+					break
+				}
+			}
+		}
+	}
+	return nodes
+}
+
+func getNodesStringRepr(nodes []*html.Node) []string {
+	var results []string
+	for _, node := range nodes {
+		results = append(results, fmt.Sprintf("%+v\n", node))
+	}
+	return results
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	images := ElementsByTagName(doc, "img")
+	fmt.Printf("%v\n", getNodesStringRepr(images))
+	headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
+	fmt.Printf("%v\n", getNodesStringRepr(headings))
+}
