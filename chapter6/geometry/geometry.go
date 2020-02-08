acecf487254769dfd072d7d384de59d8ce5bfc12
Antonio Gutierrez <chibby0ne@gmail.com>
2020-02-08 16:19:34 +0100

    ch6: add geometry
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

---
+++ chapter6/geometry/geometry.go
@@ -0,0 +1,16 @@
+package geometry
+
+import (
+	"math"
+)
+
+type Point struct{ X, Y float64 }
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
