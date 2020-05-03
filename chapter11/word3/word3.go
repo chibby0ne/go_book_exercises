2930d86edcc40d28e94cdb8d7846a526a1ee0a01
Antonio Gutierrez <chibby0ne@gmail.com>
2020-05-03 17:48:25 +0200

    ch11: word3 faster implementation with less allocations
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- chapter11/word3/word3.go
+++ chapter11/word3/word3.go
@@ -7,7 +7,7 @@ import (
 // IsPalindrome reports whether s reads the same forward and backward.
 // Letter case is ignored, as are non-letters.
 func IsPalindrome(s string) bool {
-	var letters []rune
+	letters := make([]rune, 0, len(s)) // less allocations, faster benchmark
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
